2ebf802e3406b7ae5ba763038883660177e20a41
Alwisyah Putra <alwisyah61@gmail.com>
2026-06-02 11:55:48 +0700

    THT

--- THT/src/App.java
+++ THT/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SistemGudang gudang = new SistemGudang();
+        gudang.tambahBarangBaru("B01", "Laptop", "Elektronik", 10);
+        gudang.tambahBarangBaru("B02", "Meja", "Furniture", 5);
+        gudang.tambahBarangBaru("B03", "Kursi", "Furniture", 8);
+
+        gudang.tambahStok("B01", 5);
+
+        gudang.kurangiStok("B02", 2);
+
+        gudang.kurangiStok("B03", 15);
+
+        gudang.cetakLaporan();
     }
 }
---
+++ THT/src/Barang.java
@@ -0,0 +1,13 @@
+class Barang {
+    String idBarang;
+    String namaBarang;
+    String kategori;
+    int stok;
+
+    Barang(String idBarang, String namaBarang, String kategori, int stok) {
+        this.idBarang = idBarang;
+        this.namaBarang = namaBarang;
+        this.kategori = kategori;
+        this.stok = stok;
+    }
+}
